adde410e97984e85c65c3404ea886b676e9fe7ac
Mscourtneybryant <87495361+Mscourtneybryant@users.noreply.github.com>
2024-07-20 17:10:20 -0500

    three int is solution for number 3. bitwiseOperatorsDemo is the solution for number 4. post Fix is the solution for number 5, incrementing is the solution for number 6. two int is the solution for number 7.

--- src/main/java/org/example/palindromeexmaple.java
+++ src/main/java/org/example/palindromeexmaple.java
@@ -1,11 +1,12 @@
 package org.example;
+import java.util.Scanner;
 
 public class palindromeexmaple {
     public static void main(String[] args) {
         String original, reverse = ""; // Objects of String class
-        Scanner in = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("Enter a string/number to check if it is a palindrome");
-        original = in.nextLine();
+        original = input.nextLine();
         int length = original.length();
         for ( int i = length - 1; i >= 0; i-- )
             reverse = reverse + original.charAt(i);
---
+++ src/main/java/org/example/postFix.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class postFix {
+    public static void main(String[] args) {
+        int num = 5;
+
+        System.out.println("Value before increment: " + num);
+
+        num++;
+
+        System.out.println("Value after increment: " + num);
+    }
+}
